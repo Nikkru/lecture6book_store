6f03454b2941d7bffbbd62ed1266ffd1457369fe
NikNik <56563372+Nikkru@users.noreply.github.com>
2024-01-31 20:09:46 +0300

    init

---
+++ src/main/java/com/example/lecture6book_store/Lecture6bookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.lecture6book_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture6bookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lecture6bookStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lecture6book_store/Lecture6bookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecture6book_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture6bookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
